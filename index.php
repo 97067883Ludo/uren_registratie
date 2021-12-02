68b67910fe41c424ab8a6a42c02afeec5110499e
97067883Ludo <97067883@st.deltion.nl>
2021-12-02 13:39:34 +0100

    added post protection

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 
-
-
+if (!isset($_POST['posted'])) {
+    
+}
+else{
+    $monUren = $_POST['maandagUren'];
+    $monMin = $_POST['maandagMin'];
+    $dinUren = $_POST['dinsdagUren'];
+    $dinmin = $_POST['dinsdagMin'];
+    $woenUren= $_POST['woensdagUren'];
+    $woenMin= $_POST['woensdagMin'];
+    $donUren = $_POST['donderdagUren'];
+    $donMin= $_POST['donderdagMin'];
+    $vrijUren = $_POST['vrijdagUren'];
+    $vrijMin = $_POST['vrijdagMin'];
+    //$posted = new werkweek($kjsdkf,)
+    $monMin = $monMin + ($monUren * 60);
+    $totalmon = $monUren + $monMin / 10;
+}
 
 class werkweek
 {
@@ -12,6 +28,7 @@ class werkweek
     public $vrijdag;
     function __construct($maandag, $dindsdag, $woensdag, $donderdag, $vrijdag)
     {
+
         $this->maandag = $maandag;
         $this->dindsdag = $dindsdag;
         $this->woensdag = $woensdag;
@@ -26,6 +43,7 @@ class werkweek
     {
         return "";
     }
+    
 }
 
 
@@ -36,7 +54,7 @@ class werkweek
 <html lang="en">
 
 <head>
-    <title>Bootstrap 5 Example</title>
+    <title>Uren registratie</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -56,49 +74,50 @@ class werkweek
             <div class="col-sm-4">
                 <div class="mb-3 mt-3">
                     <form action="#" method="post">
+                        <input type="hidden" name="posted" value="1">
                         <div class="row">
                             <label for="email" class="form-label">Maandag</label>
                             <div class="col">
-                                <input type="number" class="form-control" id="maandag" placeholder="Aantal uren" name="maandag">
+                                <input type="number" class="form-control" value="0" id="maandagUren" placeholder="Aantal uren" name="maandagUren">
                             </div>
                             <div class="col">
-                                <input type="number" class="form-control" id="maandag" placeholder="Aantal min" name="maandag">
+                                <input type="number" class="form-control" value="0" id="maandagMin" placeholder="Aantal min" name="maandagMin">
                             </div>
                         </div>
                         <div class="row">
                             <label for="email" class="form-label">Dinsdag</label>
                             <div class="col">
-                                <input type="number" class="form-control" id="dinsdag" placeholder="Aantal uren" name="dinsdag">
+                                <input type="number" class="form-control" value="0" id="dinsdagUren" placeholder="Aantal uren" name="dinsdagUren">
                             </div>
                             <div class="col">
-                                <input type="number" class="form-control" id="dinsdag" placeholder="Aantal min" name="dinsdag">
+                                <input type="number" class="form-control" value="0" id="dinsdagMin" placeholder="Aantal min" name="dinsdagMin">
                             </div>
                         </div>
                         <div class="row">
                             <label for="email" class="form-label">Woensdag</label>
                             <div class="col">
-                                <input type="number" class="form-control" id="woensdag" placeholder="Aantal uren" name="woensdag">
+                                <input type="number" class="form-control" value="0" id="woensdagUren" placeholder="Aantal uren" name="woensdagUren">
                             </div>
                             <div class="col">
-                                <input type="number" class="form-control" id="woensdag" placeholder="Aantal min" name="woensdag">
+                                <input type="number" class="form-control" value="0" id="woensdagMin" placeholder="Aantal min" name="woensdagMin">
                             </div>
                         </div>
                         <div class="row">
                             <label for="email" class="form-label">Donderdag</label>
                             <div class="col">
-                                <input type="number" class="form-control" id="donderdag" placeholder="Aantal uren" name="donderdag">
+                                <input type="number" class="form-control" value="0" id="donderdagUren" placeholder="Aantal uren" name="donderdagUren">
                             </div>
                             <div class="col">
-                                <input type="number" class="form-control" id="donderdag" placeholder="Aantal min" name="donderdag">
+                                <input type="number" class="form-control" value="0" id="donderdagMin" placeholder="Aantal min" name="donderdagMin">
                             </div>
                         </div>
                         <div class="row">
                             <label for="email" class="form-label">Vrijdag</label>
                             <div class="col">
-                                <input type="number" class="form-control" id="vrijdag" placeholder="Aantal uren" name="vrijdag">
+                                <input type="number" class="form-control" value="0" id="vrijdagUren" placeholder="Aantal uren" name="vrijdagUren">
                             </div>
                             <div class="col">
-                                <input type="number" class="form-control" id="vrijdag" placeholder="Aantal min" name="vrijdag">
+                                <input type="number" class="form-control" value="0" id="vrijdagMin" placeholder="Aantal min" name="vrijdagMin">
                             </div>
                         </div>
                         <div class="row">
